dbf8d5db6035868a39bd04e8ef8a7bb2d98f01c8
freakinn <rahuldhandoreeas21b0094@gmail.com>
2026-05-19 00:28:21 +0530

    sds

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://event-backend-sooty.vercel.app//api',
+    baseURL: import.meta.env.VITE_API_URL || 'https://event-backend-sooty.vercel.app/api',
 });
 
 api.interceptors.request.use((config) => {
